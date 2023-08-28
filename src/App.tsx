13e680e8cbce06343d4708f9fb283e4eb4a1b849
shakuurally <abdishakuuralimohamed@gmail.com>
2023-08-28 21:56:48 +0300

    rendering movie, filtering and fetching

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import MovieData from './components/Movie';
+
+function App() {
+  return (
+    <div className="App">
+      <MovieData />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      skeletonScreen: {
+        DEFAULT: {
+          // .loading
+          baseColor: "#c7c7c7",
+          movingColor:
+            "linear-gradient(to right, transparent 0%, #E8E8E8 50%, transparent 100%)",
+          duration: "1s",
+          timing: "cubic-bezier(0.4, 0.0, 0.2, 1)",
+        },
+        // specify another color to have multiple loading colors.
+        blue: {
+          // .loading-blue
+          baseColor: "blue",
+          movingColor:
+            "linear-gradient(to right, transparent 0%, lightblue 50%, transparent 100%)",
+          duration: ".3s",
+          timing: "ease",
+        },
+      },
+    },
+  },
+  plugins: [require("@gradin/tailwindcss-skeleton-screen")],
+};
